85ccd16011f8f46c0f7ddd4b1e038356c328f7d4
Albin Nyholm <abbe.1998@live.se>
2025-02-23 17:30:48 +0100

    Small change in python script

--- src/reservip.py
+++ src/reservip.py
@@ -4,8 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-import sys
 import getpass
 import argparse
 
